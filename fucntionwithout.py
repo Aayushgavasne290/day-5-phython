62cd5dccb1adbaca67760bbfffbd1122aa1ff1ff
Aayushgavasne290 <aayushgavasne290@gmail.com>
2026-09-17 15:13:01 +0500

    updated

--- fucntionwithout.py
+++ fucntionwithout.py
@@ -1,4 +1,4 @@
-(pi())
+
 def pi():
     return 3.14159
 print(pi())
---
+++ fuctcalci.py
@@ -0,0 +1,17 @@
+# def add(a, b):
+#     return a + b
+# print(add(5, 3))
+
+# def subtract(a, b):
+#     return a - b
+# print(subtract(5, 3))
+
+# def multiply(a, b):
+#     return a * b
+# print(multiply(5, 3))
+
+# def divide(a, b):
+#     return a / b
+# print(divide(5, 3))
+
+while True:
